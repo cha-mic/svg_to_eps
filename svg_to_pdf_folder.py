3a3a039b9bb30cf139d7cc6c783a6cf9b7178b1c
cha-mic <MITIhisa0223@gmail.com>
2024-07-03 17:39:10 +0900

    svg_to_pdf追加，画像をアップロードしないようにgitignore追加

---
+++ svg_to_pdf_folder.py
@@ -0,0 +1,34 @@
+import subprocess
+import os
+
+# Inkscapeをインストールした場所
+Inkscape_PATH = 'C:\\Program Files\\Inkscape\\bin\\inkscapecom.com'
+
+input_file_name = ''
+
+# ---- ファイル名のリスト取得 ---- #
+current_PATH = os.getcwd()
+filepath_list = os.listdir(current_PATH)
+input_file_list = []
+for filename in filepath_list:
+    if filename[-4:] == '.svg':
+        input_file_list.append(filename)
+
+# print(input_file_list)
+
+output_file_list = []
+for svg in input_file_list:
+    output_file_list.append(svg[:-4] + "_converted.pdf")
+
+print("convert to:", output_file_list)
+
+# ---- ファイル変換 ---- #
+for i in range(len(input_file_list)):
+    output_command = '--export-filename=' + output_file_list[i]
+
+    result = subprocess.run([Inkscape_PATH, output_command, input_file_list[i]])
+
+    if result.returncode == 0:
+        print("Success!")
+    else :
+        print("returncode:",result.returncode)
